53ceaba8405e60b84d6f4c32e844b1749ee92ed7
virhavasyl <52401051+virhavasyl@users.noreply.github.com>
2019-07-01 11:32:40 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -52,49 +52,6 @@ mix.combine([
 /****** END Admin Application **********/
 
 
-/**************************************
- * Frontend Application
- **************************************/
-const frontendAssets = 'Modules/Frontend/Resources/assets/';
-
-mix.copyDirectory(frontendAssets + 'img', 'public/frontend/img');
-
-mix.copyDirectory('node_modules/font-awesome/fonts', 'public/frontend/fonts');
-mix.copyDirectory('node_modules/simple-line-icons/fonts', 'public/frontend/fonts');
-mix.copyDirectory('node_modules/elegant-icons/fonts', 'public/frontend/css/fonts');
-mix.copyDirectory('node_modules/flag-icon-css/flags', 'public/frontend/flags');
-
-mix.sass(frontendAssets + 'scss/app.scss', 'public/frontend/css/app.css');
-
-mix.styles([
-  'node_modules/font-awesome/css/font-awesome.min.css',
-  'node_modules/simple-line-icons/css/simple-line-icons.css',
-  'node_modules/flag-icon-css/css/flag-icon.min.css',
-  'node_modules/elegant-icons/style.css',
-  'node_modules/bootstrap/dist/css/bootstrap.min.css',
-  'node_modules/bootstrap/dist/css/bootstrap-grid.css',
-  'node_modules/bootstrap-select/dist/css/bootstrap-select.min.css',
-  'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css',
-  'node_modules/slick-carousel/slick/slick.css',
-  'node_modules/slick-carousel/slick/slick-theme.css',
-], 'public/frontend/css/vendor.css');
-
-mix.combine([
-  'node_modules/jquery/dist/jquery.min.js',
-  frontendAssets + 'vendor/image-dropdown/jquery.dd.min.js',
-  'node_modules/popper.js/dist/umd/popper.min.js',
-  'node_modules/bootstrap/dist/js/bootstrap.min.js',
-  'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
-  'node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js',
-  'node_modules/slick-carousel/slick/slick.min.js',
-  frontendAssets + 'js/commonModule.js',
-  frontendAssets + 'js/widgets/CategoriesModule.js',
-  frontendAssets + 'js/productModule.js',
-  frontendAssets + 'js/app.js',
-  'node_modules/jquery.cookie/jquery.cookie.js',
-  frontendAssets + 'js/cartModule.js'
-], 'public/frontend/js/app.js');
-/****** END Frontend Application **********/
 
 if (mix.inProduction()) {
   mix.version();
